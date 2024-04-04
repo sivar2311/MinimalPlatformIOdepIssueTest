7f7eaadf8236d6ba017d09372f73f984d8589e8c
sivar2311 <sivar2311@gmail.com>
2024-04-04 16:39:23 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,13 @@
 #include <Arduino.h>
 #include <fileA.h>
 
-#ifdef ESP32
-#include <WiFi.h>
-#else
-#include <ESP8266WiFi.h>
-#endif
-
 const char* wifi_ssid = "";
 const char* wifi_pass = "";
-
-void setupWiFi() {
-    WiFi.begin(wifi_ssid, wifi_pass);
-    while (WiFi.status() != WL_CONNECTED) {
-        Serial.print(".");
-        delay(100);
-    }
-    Serial.printf("WiFi conncected: http://%s/\r\n", WiFi.localIP().toString().c_str());
-}
 
 void setup() {
     Serial.begin(115200);
     Serial.println("Hello World");
-    setupWiFi();
+    setupWiFi(wifi_ssid, wifi_pass);
     setupWebServer();
 }
 
